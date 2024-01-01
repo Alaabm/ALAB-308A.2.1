f91d47a8b8df1fdec04a9571a23cf968d69af118
Alaabm <alaabasim@gmail.com>
2024-01-01 12:09:41 -0500

    commit character class

--- script.js
+++ script.js
@@ -17,8 +17,27 @@
             },
             roll (mod = 0) {
                 const result = Math.floor(Math.random() * 20) + 1 + mod;
-                console.log(`${this.name} rolled a ${result}.`);
+                console.log(`${this.name} rolled a ${result}.`)
                 }
         }
     }
 }
+
+//Part 2
+class Character {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+}
+
+//Re-create Robin using Character class!
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
