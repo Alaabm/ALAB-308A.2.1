5a9a0341e8e3aad9059c6556034b1b82437458f3
Alaabm <alaabasim@gmail.com>
2024-01-01 12:23:46 -0500

    Part 3 setup

--- script.js
+++ script.js
@@ -41,3 +41,18 @@ robin.companion.companion = new Character("Frank");
 robin.companion.companion.type = "Flea";
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
+//Part 3
+class Adventurer extends Character {
+    constructor (name, role) {
+        super(name);
+      // Adventurers have specialized roles.
+        this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+        this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+    }
+}
